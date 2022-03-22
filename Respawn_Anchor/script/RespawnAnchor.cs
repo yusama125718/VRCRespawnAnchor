0538d0c23fe16e20d3bbefc594ee410ce2081176
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2022-03-22 12:18:29 +0900

    でけた

---
+++ Respawn_Anchor/script/RespawnAnchor.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using System;
+
+public class RespawnAnchor : UdonSharpBehaviour
+{
+    [Header("リスポーン地点")]
+    [SerializeField] private Transform respawnPosition = null;
+    Boolean respawn = false;
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player){
+        if (respawn){
+            Vector3 respawnVec = respawnPosition.transform.position;
+            var target = Networking.LocalPlayer;
+            target.TeleportTo(respawnVec, Quaternion.Euler(respawnVec));
+        }
+    }
+    public override void OnPlayerRespawn(VRCPlayerApi player){
+        respawn = true;
+        SendCustomEventDelayedFrames(nameof(end), 3);
+    }   
+
+    public void end(){
+        respawn = false;
+    }
+}
