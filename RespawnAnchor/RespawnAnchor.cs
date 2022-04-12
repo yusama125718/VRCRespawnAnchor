63e218247f576c49243a2bc384bff895b31e806a
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2022-04-12 21:30:03 +0900

    いろいろ改善

---
+++ RespawnAnchor/RespawnAnchor.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class RespawnAnchor : UdonSharpBehaviour
+{
+    [Header("リスポーン地点")]
+    [SerializeField] private Transform respawnPosition = null;
+    bool respawn = false;
+
+    void OnPlayerTriggerExit(VRCPlayerApi player){
+        if (respawn){
+            Vector3 respawnVec = respawnPosition.transform.position;
+            var target = Networking.LocalPlayer;
+            target.TeleportTo(respawnVec, Quaternion.Euler(respawnVec));
+        }
+    }
+    void OnPlayerRespawn(VRCPlayerApi player){
+        respawn = true;
+        SendCustomEventDelayedFrames(nameof(RAend), 3);
+    }   
+
+    public void RAend(){
+        respawn = false;
+    }
+}
